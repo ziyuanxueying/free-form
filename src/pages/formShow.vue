61fddff15159725dfc94ff3acec41f04d59da8ab
于明鑫 <mingxin.yu@naxions.com>
2022-01-26 14:23:57 +0800

    feat: v0.1.9: 添加form数据解构解释方法

--- src/pages/formShow.vue
+++ src/pages/formShow.vue
@@ -1,72 +1,34 @@
 <template>
   <div class="show-view">
-    {{ formConfig.toJSON }}
-    <Preview :formTag="form" :configList="setList"/>
-    {{ form }}
+    <!-- <Preview :form="form" :configList="setList"/> -->
   </div>
 </template>
 
 <script>
 import { reactive, toRefs, } from 'vue'
-// import { useRoute } from 'vue-router'
 import FormItem from '../components/FormItem'
 import { useFormConfigStore } from '../store'
 import Preview from './componemts/preview.vue'
-// import{ post } from '../tools/request'
 import _ from 'lodash'
+import { getForm } from '../utils'
 export default {
   components: {
     FormItem,
     Preview
   },
-  data () { 
-    return {
-
-    }
-  },
   setup () { 
     const formConfig  = useFormConfigStore()
     const state = reactive({ 
-      form:{},
       setList : _.cloneDeep(formConfig.formItemList),
-      layout:['NxTable','NxCard','NxGrid'],
     })
-    // state.setList.map(item=>{
-    //   _.merge(item, item.configList)
-    //   delete item.configList
-    //   if(state.layout.includes(item.name)) {
-    //     _.merge(item, item.layout)
-    //     delete item.layout
-    //     state.form[item.fileId] = {}
-    //   } 
-    //   console.log('form: ', state.form)
-    //   if(item.defaultVal) {
-    //     console.log(item.defaultVal)
-    //     state.form[item.fileId] = item.defaultVal
-    //   }
-      
-    //   return item
-    // })
-    
-    // const route = useRoute()
-    // console.log(route.query)
-    // const initJson = async ()=>{
-    //   let res = await post(`/formDef/get/${route.query.id}`)
-    //   formConfig.initJson(res.formDefJson)
-    // }
-    // if(route.query.id) {
-    //   initJson()
-    // }
+    let form = getForm(state.setList)
+    console.log(11111111111111,form)
     return {
       formConfig,
+      form,
       ...toRefs(state),
     }
-  },
-  mounted () {
-  },
-  methods:{
-
-  },
+  }
 }
 </script>
 
