3616ad4c95b28a606de1caca9ebab508074006c4
冯杰 <986852048@qq.com>
2022-03-08 19:28:16 +0800

    feat: v0.2.39: 修改布局组件位置

--- src/components/ItemComponents/components/module/index.js
+++ src/components/ItemComponents/components/module/index.js
@@ -1,6 +1,6 @@
 import { getModulesFiles } from '../../../../utils'
 
-const modulesFiles = require.context('../base', true, /index\.vue$/)
+const modulesFiles = require.context('../module', true, /index\.vue$/)
 const modules = getModulesFiles(modulesFiles)
 const selects = {
   name:'基础组件',
